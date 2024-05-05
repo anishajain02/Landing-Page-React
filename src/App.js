f692c9b9e944fa8339f277b63d0abb34f737bb10
sanjay-singh-patel <sanjupatel11112000@gmail.com>
2024-05-05 09:40:40 +0530

    Coming Soon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const gradient = keyframes`
+  0% {background-position: 0% 50%;}
+  50% {background-position: 100% 50%;}
+  100% {background-position: 0% 50%;}
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  color: #EAE7E2;
+  background: linear-gradient(-45deg, #405958, #5A7974, #87938D, #AFA086, #99B6B6, #547572, #95B5AD, #6C8D84, #6D8D8B);
+  background-size: 1800% 1800%;
+  animation: ${gradient} 15s ease infinite;
+`;
+
+const Title = styled.h1`
+  font-size: 3em;
+  margin-bottom: 20px;
+  color: #EAE7E2;
+`;
+
+const Subtitle = styled.p`
+  font-size: 1.5em;
+  color: #EAE7E2;
+`;
+
+const Tagline = styled.p`
+  font-size: 1em;
+  font-style: italic;
+  color: #EAE7E2;
+`;
+
+const App = () => (
+  <Container>
+    <Title>Six Pockets</Title>
+    <Subtitle>Coming Soon...</Subtitle>
+    <Tagline>The ultimate destination for snooker club owners and managers</Tagline>
+  </Container>
+);
 
 export default App;
